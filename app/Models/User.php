754edfa1a2253c234b08f38d1d86616a496602b7
dwinz14 <dwinz1214@gmail.com>
2025-09-30 11:18:26 +0700

    fix : user last login

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,8 @@ class User extends Authenticatable
         'password',
         'role',
         'division_id',
-        'sisa_cuti'
+        'sisa_cuti',
+        'last_login_at'
     ];
 
     /**
