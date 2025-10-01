7404667e892cb0c349457e83e2157b7f74cda73d
dwinz14 <dwinz1214@gmail.com>
2025-10-01 09:20:20 +0700

    fix : user management

--- config/menu.php
+++ config/menu.php
@@ -87,7 +87,7 @@ return [
             'active_pattern' => 'approval.history*'
         ],
         [
-            'name' => 'User Activity',
+            'name' => 'User Managment',
             'route' => 'admin.user-activity.index',
             'icon' => '<svg class="mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
