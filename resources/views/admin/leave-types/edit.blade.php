22766e144804c1ba2ca85afea2781ac7c554fe4a
dwinz14 <dwinz1214@gmail.com>
2025-11-12 17:06:25 +0700

    UI/UX

--- resources/views/admin/leave-types/edit.blade.php
+++ resources/views/admin/leave-types/edit.blade.php
@@ -53,10 +53,7 @@
 
                 <div class="flex items-center justify-end space-x-3">
                     <a href="{{ route('admin.leave-types.index') }}"
-                        class="inline-flex items-center justify-center px-4 py-2 bg-red-400 dark:bg-slate-600 border border-gray-300 dark:border-gray-500
-               rounded-full font-semibold text-xs text-slate-50 dark:text-gray-200 uppercase tracking-widest shadow-sm
-               hover:bg-rose-300 dark:hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-indigo-500
-               focus:ring-offset-2 dark:ring-offset-slate-800 transition ease-in-out duration-150">
+                        class="inline-flex items-center justify-center px-4 py-2 bg-red-400 dark:bg-slate-600 border border-gray-300 dark:border-gray-500 rounded-full font-semibold text-xs text-slate-50 dark:text-gray-200 uppercase tracking-widest shadow-sm hover:bg-rose-300 dark:hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:ring-offset-slate-800 transition ease-in-out duration-150">
                         Batal
                     </a>
 
